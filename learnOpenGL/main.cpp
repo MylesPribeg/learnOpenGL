240247241ce66c0fd7912d6c44eafa3afd49958c
Myles Pribeg <mylespribeg@gmail.com>
2022-05-03 17:20:16 -0600

    Multicolor triangle

--- learnOpenGL/main.cpp
+++ learnOpenGL/main.cpp
@@ -10,20 +10,20 @@ static void GLAPIENTRY errorOccurredGL(GLenum source, GLenum type, GLuint id, GL
 // SHADERS
 const char* vertexShaderSrc = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos; \n "
-//"out vec4 vertexColor;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 vertexColor;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(aPos.x, aPos.y, aPos.x, 1.0);\n"
-//"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"	vertexColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSrc = "#version 330 core\n"
 "out vec4 FragColor;\n"
-//"in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 vertexColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = ourColor;\n"
+"	FragColor = vec4(vertexColor, 1.0f);\n"
 "}\n";
 
 
@@ -113,17 +113,18 @@ int main() {
 
 	// Triangle Information
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,	// top right
-		 0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f, // top left
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 
 	};
+	/*
 	unsigned int indices[] = {
 		0, 1, 3,  // first triangle
 		1, 2, 3   // second triangle
 	};
-
+	*/
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -133,17 +134,21 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
+	/*
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // telling how to interpret vertex data
+	*/
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // telling how to interpret vertex data
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindVertexArray(0);
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -156,18 +161,17 @@ int main() {
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		/*
 		float timeValue = glfwGetTime();
 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-
+		*/
 		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 				
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		
 		// show
